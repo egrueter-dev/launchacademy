d2265309a3b655c10ff867dd283215dc4f1ffd44
Erik <erikgrueter@gmail.com>
2014-12-19 20:26:22 +0100

    some programs written for launch acad

---
+++ launchacad.rb
@@ -0,0 +1,95 @@
+
+#self 
+
+# puts self
+
+
+# def modulo_understanding num
+# 	roman = '' 
+# 	roman << num.to_s
+# 	puts roman
+# end
+
+# modulo_understanding 80
+
+#see ruby string documentation 
+
+
+#recursion problem <<<<<<<<<
+
+ # def append2(n)
+ # 	ary = []
+ # 	n.downto(0) { |i| ary << i }
+ #  	puts ary
+ # end
+
+#  def append(ary,n)
+#  	if n > 0
+#  	n.downto(0) { |i| ary << i }
+#  #	final = append([],n-1)
+#  	end
+#  	puts ary
+#  end
+
+# append [], 10
+
+
+
+
+# Grandma problem <<<<<<<<
+
+
+# puts "Say Something to Grandma"
+# bye = 0
+
+# while bye < 3
+# 	reply = gets.chomp
+# 	if reply == "BYE"
+# 		bye += 1
+# 	elsif reply == reply.upcase #condition, not variable 
+# 		puts "No, not since #{rand(1930..1950)}"
+# 	else
+# 		puts "What?????"
+# 	end
+# end
+
+
+# def english_number english_number
+# 	if number < 0
+# 		return 'Please enter a number zero or greater'
+# 	end
+# 	if number > 100
+# 		return 'Please enter a number 100 or less'
+# 	end
+
+# 	ones_place = {ones_place => "one" }
+
+
+# end
+
+
+#how does this code know which value in the hash? should be reversed? 
+
+# puts "Please provide a sentence" 
+# text = gets.chomp
+# words = text.split
+# frequencies = Hash.new(0)
+# puts h
+# words.each do |word|
+#     frequencies[word] += 1
+# end
+# frequencies = frequencies.sort_by do |word, frequency|
+#     frequency
+# end
+# frequencies.reverse!
+
+# frequencies.each do |word, frequency|
+# puts "#{word} #{frequency}"
+# end
+
+
+
+
+		
+
+
